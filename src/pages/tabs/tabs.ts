1327fbad8a660c74c01fb9b00b9f1f88dbc43ba5
shery11 <shery_kirmani@hotmail.com>
2017-11-06 22:50:08 +0500

    first

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,14 +4,18 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
+import { NewpagePage } from '../newpage/newpage';
+
+
 @Component({
-  templateUrl: 'tabs.html'
+   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = NewpagePage
 
   constructor() {
 
